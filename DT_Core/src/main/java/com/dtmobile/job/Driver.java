569fe54c4ec2d28e3ef41122377c0d98735e361b
alexskyer <alexskyer@163.com>
2017-04-07 11:54:57 +0800

    【SSH HELP INIT】

--- DT_Core/src/main/java/com/dtmobile/job/Driver.java
+++ DT_Core/src/main/java/com/dtmobile/job/Driver.java
@@ -12,17 +12,13 @@ import com.dtmobile.util.SSHHelper;
  **/
 
 public class Driver {
-    public static void main(String[] args) {
-      /*  AzkabanOperator op = new AzkabanOperator(args[0], args[1], args[2], args[3], args[4], args[5],args[6], args[7]);
-        try {
-            JSONObject json = op.login();
-            System.out.println(JSONObject.toJSONString(json));
-            System.out.println(json.getString("session.id"));
-            System.out.println(JSONObject.toJSONString(op.executeGdiFlow(json.getString("session.id"), args[8], args[9])));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-        String exec = SSHHelper.exec("namenode01", "hadoop", "hadoop", 22, "/home/hadoop/hdfs2local.sh;ls /home/hadoop");
+    public static void main(String[] args) throws Exception {
+        String exec = SSHHelper.exec(args[10], args[11], args[12], Integer.valueOf(args[13]),args[14]);
         System.out.println(exec);
+        AzkabanOperator op = new AzkabanOperator(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+        JSONObject json = op.login();
+        System.out.println(JSONObject.toJSONString(json));
+        System.out.println(json.getString("session.id"));
+        System.out.println(JSONObject.toJSONString(op.executeGdiFlow(json.getString("session.id"), args[8], args[9])));
     }
 }
--- DT_Liaoning/shell/bin/crontab.sh
+++ DT_Liaoning/shell/bin/crontab.sh
@@ -4,32 +4,32 @@ export HADOOP_HOME=/opt/app/hadoop
 export SPARK_HOME=/opt/app/spark
 export HADOOP_CONF_DIR=/opt/app/hdconf
 export PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin::$SPARK_HOME/bin
-ANALY_DATE=`date +%Y%m%d`
-ANALY_HOUR="`date -d ' -0 hour' +%H`"
-
+mypath="$(cd "$(dirname "$0")"; pwd)"
+cd $mypath
 JAR=/dt/lib/DT_Core-1.0-SNAPSHOT.jar
-URL=https://172.30.4.222:8443
+URL=https://fg03:8443
 USERNAME=azkaban
 PASSWD=azkaban
-PROJECT=Test
-FLOW=JobComplete
+PROJECT=Analy
+FLOW=JobAnaly
 KEYSTORE=/opt/app/azkaban/web/conf/keystore
-mkdir -p /data1/taiyue/TEMP/volte_orgn/${ANALY_DATE}/${ANALY_HOUR}/
-mkdir -p /data1/taiyue/TEMP/volte_rx/${ANALY_DATE}/${ANALY_HOUR}/
-mkdir -p /data1/taiyue/TEMP/volte_sv/${ANALY_DATE}/${ANALY_HOUR}/
-mkdir -p /data1/taiyue/TEMP/s1mme_orgn/${ANALY_DATE}/${ANALY_HOUR}/
-mkdir -p /data1/taiyue/TEMP/s1u_http_orgn/${ANALY_DATE}/${ANALY_HOUR}/
-mkdir -p  /appfs/hadoopfile/LTE_MRO_SOURCE/${ANALY_DATE}/${ANALY_HOUR}/
-mkdir -p  /appfs/hadoopfile/TB_XDR_IFC_UU/${ANALY_DATE}/${ANALY_HOUR}/
-mkdir -p  /appfs/hadoopfile/TB_XDR_IFC_X2/${ANALY_DATE}/${ANALY_HOUR}/
 
-hdfs dfs -get /datang/volte_orgn/20170227/08/*     /data1/taiyue/TEMP/volte_orgn/${ANALY_DATE}/${ANALY_HOUR}/
-hdfs dfs -get /datang/volte_rx/20170227/08/*       /data1/taiyue/TEMP/volte_rx/${ANALY_DATE}/${ANALY_HOUR}/
-hdfs dfs -get /datang/volte_sv/20170227/08/*       /data1/taiyue/TEMP/volte_sv/${ANALY_DATE}/${ANALY_HOUR}/
-hdfs dfs -get /datang/s1mme_orgn/20170227/08/*     /data1/taiyue/TEMP/s1mme_orgn/${ANALY_DATE}/${ANALY_HOUR}/
-hdfs dfs -get /datang/s1u_http_orgn/20170227/08/*  /data1/taiyue/TEMP/s1u_http_orgn/${ANALY_DATE}/${ANALY_HOUR}/
-hdfs dfs -get /datang/LTE_MRO_SOURCE/20170227/08/*   /appfs/hadoopfile/LTE_MRO_SOURCE/${ANALY_DATE}/${ANALY_HOUR}/
-hdfs dfs -get /datang/TB_XDR_IFC_UU/20170227/08/*   /appfs/hadoopfile/TB_XDR_IFC_UU/${ANALY_DATE}/${ANALY_HOUR}/
-hdfs dfs -get /datang/TB_XDR_IFC_X2/20170227/08/*   /appfs/hadoopfile/TB_XDR_IFC_X2/${ANALY_DATE}/${ANALY_HOUR}/
+ANALY_DATE=`date +%Y%m%d`
+ANALY_HOUR="`date -d ' -1 hour' +%H`"
+CUR_DATE=`date +%Y%m%d`
+CUR_HOUR="`date -d ' -0 hour' +%H`"
+ANALY_DATE=$CUR_DATE
+if [ $CUR_HOUR = 00 ]
+then
+   ANALY_DATE="`date -d ' -1 day' +%Y%m%d`"
+else
+   ANALY_DATE=$CUR_DATE
+fi
+HOST=taiyue
+HUSERNAME=datang
+HPASSWD=Datang@12
+PORT=50072
+CMD= /cup/d4/datang/bin/hdfs2local.sh;ls /cup/d4/datang/TEMP/s1mme_orgn/$ANALY_DATE/$ANALY_HOUR
 
-java -jar $JAR $URL $USERNAME $PASSWD $PROJECT $FLOW $PASSWD $KEYSTORE $KEYSTORE $ANALY_DATE $ANALY_HOUR
+java -jar $JAR $URL $USERNAME $PASSWD $PROJECT $FLOW $PASSWD $KEYSTORE $KEYSTORE $ANALY_DATE $ANALY_HOUR \
+$HOST $HUSERNAME $HPASSWD $PORT $CMD
