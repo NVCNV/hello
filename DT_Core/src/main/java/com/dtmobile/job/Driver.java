0d5db1d6a3689cac3112bb6ada916ce0a18f9400
zhangchao15 <abcd.1234>
2017-04-12 09:27:59 +0800

    【ssh main azkaban main】

--- DT_Core/src/main/java/com/dtmobile/job/Driver.java
+++ DT_Core/src/main/java/com/dtmobile/job/Driver.java
@@ -4,6 +4,9 @@ import com.alibaba.fastjson.JSONObject;
 import com.dtmobile.azkaban.AzkabanOperator;
 import com.dtmobile.util.SSHHelper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Driver
  *
@@ -13,14 +16,20 @@ import com.dtmobile.util.SSHHelper;
 
 public class Driver {
     public static void main(String[] args) throws Exception {
-        String exec = SSHHelper.exec(args[10], args[11], args[12], Integer.valueOf(args[13]),args[14]);
-        System.out.println(exec);
-        /*
-        AzkabanOperator op = new AzkabanOperator(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
-        JSONObject json = op.login();
-        System.out.println(JSONObject.toJSONString(json));
-        System.out.println(json.getString("session.id"));
-        System.out.println(JSONObject.toJSONString(op.executeGdiFlow(json.getString("session.id"), args[8], args[9])));
-        */
+        if(args.length==5){
+            String exec = SSHHelper.exec(args[10], args[11], args[12], Integer.valueOf(args[13]),args[14]);
+            System.out.println(exec);
+        }else{
+            AzkabanOperator op = new AzkabanOperator(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args [7]);
+            JSONObject json = op.login();
+            System.out.println(JSONObject.toJSONString(json));
+            System.out.println(json.getString("session.id"));
+            Map<String, String> paramMap = new HashMap<String, String>();
+            for (int i = 9; i < args.length; i++){
+                paramMap.put(args[i], args[i++]);
+                i++;
+            }
+            System.out.println(JSONObject.toJSONString(op.executeGdiFlow(json.getString("session.id"),paramMap)));
+        }
     }
 }
