0a0226b7e231386afb289ba06a75e6f4f84328f8
shenkaili <shenkaili@datangmobile.cn>
2017-04-21 09:41:47 +0800

    【Bussiness INIT】

--- DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businessexception/businessexception.scala
+++ DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businessexception/businessexception.scala
@@ -117,7 +117,7 @@ class businessexception (ANALY_DATE: String,ANALY_HOUR: String,SDB: String, DDB:
      sql(
        s"""
           |select city,xdrid,procedurestarttime,from_unixtime(cast(round(procedurestarttime/1000) as int)),procedureendtime,imsi,imei,substring(imei,1,8)TEtac,msisdn,
-          |ecgi,sgwipaddr,appserveripipv4,apptype,appsubtype,appstatus,failtype,errorcode,
+          |ecgi,sgwipaddr,appserveripipv4,apptype,appsubtype,appstatus,etype,errorcode,
           |(case when errorcode="1" then "11"
           |when errorcode="2" then "22"
           |when errorcode="3" or errorcode="4" then "33"
@@ -159,7 +159,7 @@ class businessexception (ANALY_DATE: String,ANALY_HOUR: String,SDB: String, DDB:
           |      when (apptype=1 and appstatus=0 and busrede>${XDRthreshold05}) then "5"
           |      when (apptype=1 and appstatus=0 and (dldata*8/(case when httplastrede-httpfirstrede<10 then 10 else httplastrede-httpfirstrede end)*1000)>${XDRthreshold06}) then "6"
           |      end
-          |)failtype
+          |)etype
           |from (select * from tb_xdr_ifc_http where dt="$ANALY_DATE" and h="$ANALY_HOUR" and
           |(httpstate>=400 or
           |(apptype=15 and appstatus=0 and busrede>${XDRthreshold01}) or
