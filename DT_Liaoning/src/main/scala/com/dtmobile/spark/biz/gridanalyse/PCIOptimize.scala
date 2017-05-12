1be3b444956f217cbcbd2753a1719adf98b03dd8
zhoudehu <zhoudehu@datangmobile.cn>
2017-05-12 14:18:58 +0800

    [alter table partition]

--- DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/gridanalyse/PCIOptimize.scala
+++ DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/gridanalyse/PCIOptimize.scala
@@ -51,8 +51,9 @@ class PCIOptimize(ANALY_DATE: String, ANALY_HOUR: String, SDB: String, DDB: Stri
     val sRsrpLap = t.get(0).getString(0)+" "+t.get(0).getDecimal(1)
     val t2 = sql("select operator,value from ltepci_degree_condition where field = 'adjcellrsrpeffectiveth'").collectAsList()
     val adjRsrpthresh = t.get(0).getString(0)+" "+t.get(0).getDecimal(1)
-    sql("use morpho")
-    sql(s"""alter table LTE_MRO_DISTURB_PRETREATE60 add if not exists partition(dt=$ANALY_DATE,h=$ANALY_HOUR)""".stripMargin)
+    sql(s"use $DDB")
+    sql(s"""alter table LTE_MRO_DISTURB_PRETREATE60 drop if  exists partition(dt=$ANALY_DATE,h=$ANALY_HOUR)""".stripMargin)
+    sql(s"""alter table LTE_MRO_DISTURB_PRETREATE60 add  partition(dt=$ANALY_DATE,h=$ANALY_HOUR)""".stripMargin)
     val selectSql=s"""select t.startTime, t.endTime, t.timeseq,
                       |        t.mmecode, t.enbid, t.cellid,t2.cellname,
                       |        (case when t.kpi10!= -1 then t.kpi10 else null end) as cellpci,
