b4256f500768cdee153f454b71293bbaa2d36d39
zhoudehu <zhoudehu@datangmobile.cn>
2017-05-19 14:07:26 +0800

    [add new package]

---
+++ DT_Shanxi/src/main/scala/com/dtmobile/spark/job/AnalyJob.scala
@@ -0,0 +1,26 @@
+package com.dtmobile.spark.job
+
+import com.dtmobile.spark.Analyse
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by zhoudehu on 2017/5/19/0019.
+  */
+class AnalyJob(args: Array[String]) extends Analyse {
+  override val appName: String = this.getClass.getName
+  override val master: String = args(4)
+  override val sourceDir: String = args(6)
+  val warhouseDir: String = "/user/hive/warehouse/" + args(3) + ".db"
+  override def analyse(implicit sparkSession: SparkSession): Unit = {
+
+
+  }
+
+  object AnalyJob {
+    def main(args: Array[String]): Unit = {
+      val analyJob = new AnalyJob(args: Array[String])
+      analyJob.exec()
+    }
+  }
+
+}
