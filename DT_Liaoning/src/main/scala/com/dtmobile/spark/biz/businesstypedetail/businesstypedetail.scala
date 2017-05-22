a78aee86a833c8b4e2c83772daadfc8b40b699e3
weiyaqin <weiyaqin@datangmobile.cn>
2017-05-22 15:04:00 +0800

    modify business type analysis

--- DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businesstypedetail/businesstypedetail.scala
+++ DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businesstypedetail/businesstypedetail.scala
@@ -21,6 +21,7 @@ class businesstypedetail (ANALY_DATE: String,ANALY_HOUR: String,SDB: String, DDB
     sql(s"use $DDB")
     sql(s"""alter table business_type_detail add if not exists partition(dt=$ANALY_DATE,h=$ANALY_HOUR)
     LOCATION 'hdfs://dtcluster/$warhouseDir/business_type_detail/dt=$ANALY_DATE/h=$ANALY_HOUR'""")
+ /*
     sql(
       s"""
          |Select "$CAL_DATE",t1.city,t2.REGION,t1.ecgi, t1.apptype, t1.appsubtype, sum(uldata),sum(dldata) from tb_xdr_ifc_http t1
@@ -28,6 +29,16 @@ class businesstypedetail (ANALY_DATE: String,ANALY_HOUR: String,SDB: String, DDB
          |where dt="$ANALY_DATE" and h="$ANALY_HOUR"
          |group by
          |t1.city,t2.REGION,t1.ecgi,t1.apptype, t1.appsubtype
+       """.stripMargin).write.mode(SaveMode.Overwrite).csv(s"$warhouseDir/business_type_detail/dt=$ANALY_DATE/h=$ANALY_HOUR")
+*/
+    sql(
+      s"""
+         | Select "$CAL_DATE",t1.city,t2.REGION,t1.apptype, t1.appsubtype, sum(uldata),sum(dldata),count(*),
+         | sum(procedureendtime-procedurestarttime) from tb_xdr_ifc_http t1
+         | inner join ltecell t2 on t1.ecgi=t2.cellid
+         | where dt="$ANALY_DATE" and h="$ANALY_HOUR"
+         | group by
+         | t1.city,t2.REGION,t1.apptype, t1.appsubtype
        """.stripMargin).write.mode(SaveMode.Overwrite).csv(s"$warhouseDir/business_type_detail/dt=$ANALY_DATE/h=$ANALY_HOUR")
   }
 }
