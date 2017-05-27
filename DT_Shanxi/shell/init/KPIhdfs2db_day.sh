8e99fe28dbaf4a7174da425344de7ff98203b866
gelongqiang <gelongqiang@datangmobile.cn>
2017-05-27 17:12:32 +0800

    [alter the === error]

--- DT_Shanxi/shell/init/KPIhdfs2db_day.sh
+++ DT_Shanxi/shell/init/KPIhdfs2db_day.sh
@@ -4,6 +4,18 @@ ANALY_DATE=$1
 DATABASE=$2
 DIR=hdfs://dtcluster/user/hive/warehouse/${DATABASE}.db
 
+echo "sh VolumeAnalyseHDFS2db.sh ${DIR}/KPI_IMSI_COLS/dt=${ANALY_DATE}  35 2"
+sh VolumeAnalyseHDFS2db.sh ${DIR}/KPI_IMSI_COLS/dt=${ANALY_DATE}  35 2 &
+
+echo "sh VolumeAnalyseHDFS2db.sh ${DIR}/KPI_CELL_COLS/dt=${ANALY_DATE}  36 2"
+sh VolumeAnalyseHDFS2db.sh ${DIR}/KPI_CELL_COLS/dt=${ANALY_DATE} 36 2 &
+
+echo "sh VolumeAnalyseHDFS2db.sh ${DIR}/KPI_MR_IMSI_COLS/dt=${ANALY_DATE}  38 2"
+sh VolumeAnalyseHDFS2db.sh ${DIR}/KPI_MR_IMSI_COLS/dt=${ANALY_DATE} 38 2 &
+
+echo "sh VolumeAnalyseHDFS2db.sh ${DIR}/KPI_MR_CELL_COLS/dt=${ANALY_DATE}  39 2"
+sh VolumeAnalyseHDFS2db.sh ${DIR}/KPI_MR_CELL_COLS/dt=${ANALY_DATE}  39 2 &
+
 sh VolumeAnalyseHDFS2db.sh ${DIR}/tac_day_http/dt=${ANALY_DATE} 13 2
 
 sh VolumeAnalyseHDFS2db.sh ${DIR}/cell_day_http/dt=${ANALY_DATE} 15 2
