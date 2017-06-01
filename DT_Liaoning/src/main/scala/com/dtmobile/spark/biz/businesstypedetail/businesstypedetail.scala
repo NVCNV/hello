926bc60c7458f749a46edfbf705e94d3e4d4e359
weiyaqin <weiyaqin@datangmobile.cn>
2017-06-01 14:44:43 +0800

    add succCnt、dltime field

--- DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businesstypedetail/businesstypedetail.scala
+++ DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businesstypedetail/businesstypedetail.scala
@@ -40,7 +40,12 @@ class businesstypedetail (ANALY_DATE: String,ANALY_HOUR: String,SDB: String, DDB
     sql(
       s"""
          | Select "$CAL_DATE",t2.city,t2.REGION,t1.apptype, t1.appsubtype, sum(uldata),sum(dldata),count(*),
-         | sum(procedureendtime-procedurestarttime) from tb_xdr_ifc_http t1
+         | sum(procedureendtime-procedurestarttime),sum(appstatusxdr),sum(rede) from
+         | (select *, (case when appstatus=0 then 1 else 0 end) appstatusxdr,
+         | (case when httpfirstrede is null then 0
+         | when httplastrede is null then httpfirstrede
+         | when (httplastrede-httpfirstrede)<=10 then httpfirstrede
+         | else (httplastrede-httpfirstrede) end) rede from tb_xdr_ifc_http a) t1
          | inner join ltecell t2 on t1.ecgi=t2.cellid
          | where dt="$ANALY_DATE" and h="$ANALY_HOUR"
          | group by
