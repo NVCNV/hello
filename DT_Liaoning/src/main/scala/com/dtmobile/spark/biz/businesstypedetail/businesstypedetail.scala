c62d95620d141f620ebaa62cf643b5b5567f9314
weiyaqin <weiyaqin@datangmobile.cn>
2017-06-02 13:49:34 +0800

    add fields: resptime, respsucccnt

--- DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businesstypedetail/businesstypedetail.scala
+++ DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businesstypedetail/businesstypedetail.scala
@@ -40,12 +40,14 @@ class businesstypedetail (ANALY_DATE: String,ANALY_HOUR: String,SDB: String, DDB
     sql(
       s"""
          | Select "$CAL_DATE",t2.city,t2.REGION,t1.apptype, t1.appsubtype, sum(uldata),sum(dldata),count(*),
-         | sum(procedureendtime-procedurestarttime),sum(appstatusxdr),sum(rede) from
+         | sum(procedureendtime-procedurestarttime),sum(appstatusxdr),sum(rede),sum(httpfirstrede), sum(recnt) from
          | (select *, (case when appstatus=0 then 1 else 0 end) appstatusxdr,
          | (case when httpfirstrede is null then 0
          | when httplastrede is null then httpfirstrede
          | when (httplastrede-httpfirstrede)<=10 then httpfirstrede
-         | else (httplastrede-httpfirstrede) end) rede from tb_xdr_ifc_http a) t1
+         | else (httplastrede-httpfirstrede) end) rede,
+         | (case when (httpfirstrede is null or httpfirstrede=0 or httpfirstrede=4294967295 ) then 0 else 1 end) recnt
+         | from tb_xdr_ifc_http a) t1
          | inner join ltecell t2 on t1.ecgi=t2.cellid
          | where dt="$ANALY_DATE" and h="$ANALY_HOUR"
          | group by
