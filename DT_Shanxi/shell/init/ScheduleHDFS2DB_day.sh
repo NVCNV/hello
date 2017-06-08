c2d9b1e7db577217546470059582cb34798c0a83
gelongqiang <gelongqiang@datangmobile.cn>
2017-06-08 10:47:50 +0800

    [add param]

--- DT_Shanxi/shell/init/ScheduleHDFS2DB_day.sh
+++ DT_Shanxi/shell/init/ScheduleHDFS2DB_day.sh
@@ -2,16 +2,17 @@
 export HADOOP_CONF_DIR=/opt/app/hdconf
 ANALY_DATE=$1
 DATABASE=$2
+OracleADDR=$3
 DIR=hdfs://dtcluster/user/hive/warehouse/${DATABASE}.db
 
-sh VolumeAnalyseHDFS2db.sh ${DIR}/gt_freq_baseday/dt=${ANALY_DATE} 8 2
+sh VolumeAnalyseHDFS2db.sh ${OracleADDR} ${DIR}/gt_freq_baseday/dt=${ANALY_DATE} 8 2
 
-sh VolumeAnalyseHDFS2db.sh ${DIR}/gt_balence_baseday/dt=${ANALY_DATE} 28 2
+sh VolumeAnalyseHDFS2db.sh ${OracleADDR} ${DIR}/gt_balence_baseday/dt=${ANALY_DATE} 28 2
 
-sh VolumeAnalyseHDFS2db.sh ${DIR}/gt_shorttimelen_baseday/dt=${ANALY_DATE} 29 2
+sh VolumeAnalyseHDFS2db.sh ${OracleADDR} ${DIR}/gt_shorttimelen_baseday/dt=${ANALY_DATE} 29 2
 
-sh VolumeAnalyseHDFS2db.sh ${DIR}/gt_overtimelen_baseday/dt=${ANALY_DATE} 30 2
+sh VolumeAnalyseHDFS2db.sh ${OracleADDR} ${DIR}/gt_overtimelen_baseday/dt=${ANALY_DATE} 30 2
 
-sh VolumeAnalyseHDFS2db.sh ${DIR}/gt_commusermore_baseday/dt=${ANALY_DATE} 31 2
+sh VolumeAnalyseHDFS2db.sh ${OracleADDR} ${DIR}/gt_commusermore_baseday/dt=${ANALY_DATE} 31 2
 
-sh VolumeAnalyseHDFS2db.sh ${DIR}/gt_highattach_baseday/dt=${ANALY_DATE} 32 2
+sh VolumeAnalyseHDFS2db.sh ${OracleADDR} ${DIR}/gt_highattach_baseday/dt=${ANALY_DATE} 32 2
