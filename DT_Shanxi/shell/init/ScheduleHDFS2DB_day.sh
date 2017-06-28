d81d95caf88ab9ca303f83908dad90b42605bb88
gelongqiang <gelongqiang@datangmobile.cn>
2017-06-28 18:40:17 +0800

    [change CRLF to LF]

--- DT_Shanxi/shell/init/ScheduleHDFS2DB_day.sh
+++ DT_Shanxi/shell/init/ScheduleHDFS2DB_day.sh
@@ -18,4 +18,3 @@ sh ${VAADDR}/VolumeAnalyseHDFS2db.sh ${OracleADDR} ${DIR}/gt_overtimelen_baseday
 sh ${VAADDR}/VolumeAnalyseHDFS2db.sh ${OracleADDR} ${DIR}/gt_commusermore_baseday/dt=${ANALY_DATE} 31 2
 
 sh ${VAADDR}/VolumeAnalyseHDFS2db.sh ${OracleADDR} ${DIR}/gt_highattach_baseday/dt=${ANALY_DATE} 32 2
-
