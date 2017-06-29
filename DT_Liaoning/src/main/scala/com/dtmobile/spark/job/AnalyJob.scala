283f27b82b929985667543390794798a00079992
zhoudehu <zhoudehu@datangmobile.cn>
2017-06-29 17:33:48 +0800

    [time】

--- DT_Liaoning/src/main/scala/com/dtmobile/spark/job/AnalyJob.scala
+++ DT_Liaoning/src/main/scala/com/dtmobile/spark/job/AnalyJob.scala
@@ -29,13 +29,13 @@ class AnalyJob(args: Array[String]) extends Analyse {
     val nsspAnaly = new NsspAnaly(args(0), args(1), args(2), args(3), sourceDir, warhouseDir)
     val kpiHourAnaly = new KpiHourAnaly(args(0), args(1), args(2), args(3), warhouseDir,onoff)
     val kpibusinessHourAnaly = new KpibusinessHourAnaly(args(0), args(1), args(2), args(3), warhouseDir,onoff)
-    val kpibusinessDayAnaly = new KpibusinessDayAnaly(args(0), args(2), args(3), warhouseDir)
+
     val exception=new businessexception(args(0),args(1), args(2), args(3), warhouseDir,args(5))
     val typedetail=new businesstypedetail(args(0),args(1), args(2), args(3), warhouseDir)
     nsspAnaly.analyse
-//    kpibusinessHourAnaly.analyse
+    kpibusinessHourAnaly.analyse
 
-    kpiHourAnaly.analyse
+//    kpiHourAnaly.analyse
     exception.analyse
     typedetail.analyse
 
@@ -75,7 +75,8 @@ class AnalyJob(args: Array[String]) extends Analyse {
     }
 */
     if("03".equals(args(1))){
-    val kpiDayAnALY = new KpiDayAnaly(DateUtils.addDay(args(0), -1, "yyyyMMdd"), args(2), args(3), warhouseDir)
+//    val kpiDayAnALY = new KpiDayAnaly(DateUtils.addDay(args(0), -1, "yyyyMMdd"), args(2), args(3), warhouseDir)
+      val kpibusinessDayAnaly = new KpibusinessDayAnaly(DateUtils.addDay(args(0), -1, "yyyyMMdd"), args(2), args(3), warhouseDir)
 //      kpiDayAnALY.analyse
       kpibusinessDayAnaly.analyse
     }
