a6e60ff63d459cd72247dcb2e1c5e16dccd0febb
gelongqiang <gelongqiang@datangmobile.cn>
2017-07-07 20:37:30 +0800

    [KPI SQLLDR]

---
+++ DT_Shanxi/shell/init/bin/bushu/kpi_sqlldr2db_day.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export ORALCE_HOME=/opt/oracle
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+export PATH=$PATH:$LD_LIBRARY_PATH
+
+TAKING_DATE=$1
+mypath="$("$(dirname; "$0")";pwd)"
+cd $mypath
+
+HDFS_ADDR='hdfs://dtcluster/user/hive/warehouse/result.db'
+LOCAL_ADDR='/dt/NewData'
+ORACLE_ADDR='userid=scott/tiger@hadoop'
+LOG_ADDR='/dt/sqllog'
+CTL_ADDR='/dt/ctl'
+HIVE_TBLES='tac_day_http cell_day_http sp_day_http ue_day_http imsi_cell_day_http sgw_day_http'
+
+rm -rf ${LOCAL_ADDR}/${TAKING_DATE}
+mkdir ${LOCAL_ADDR}/${TAKING_DATE}
+
+for tableName in ${HIVE_TBLES}
+do
+	mkdir -p ${LOG_ADDR}/${tableName}/${TAKING_DATE}
+	echo "getmerge from ${HDFS_ADDR}/${TAKING_DATE}/ to ${LOCAL_ADDR}/${TAKING_DATE}/${tableName}.dat"
+	hdfs dfs -getmerge ${HDFS_ADDR}/${tableName}/dt=${TAKING_DATE}/* ${LOCAL_ADDR}/${TAKING_DATE}/${tableName}.dat
+	echo "load ${LOCAL_ADDR}/${TAKING_DATE}/${TAKING_HOUR}/${tableName}.dat to  oralce ${tableName}"
+	sqlldr ${ORACLE_ADDR} control=${CTL_ADDR}/${tableName}.ctl data=${LOCAL_ADDR}/${TAKING_DATE}/${tableName}.dat log=${LOG_ADDR}/${tableName}/${TAKING_DATE}/${TAKING_DATE}.log
+done
