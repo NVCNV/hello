779ab8993b0ca741e1c6c0ac4586a18c29e1e574
gelongqiang <gelongqiang@datangmobile.cn>
2017-07-08 12:18:18 +0800

    [delete export]

--- DT_Shanxi/shell/init/Kpi2db_hour.sh
+++ DT_Shanxi/shell/init/Kpi2db_hour.sh
@@ -11,7 +11,7 @@ LOCAL_ADDR='/dt/NewData'
 ORACLE_ADDR="userid=scott/tiger@${ORACLEDB}"
 LOG_ADDR='/dt/sqllog'
 CTL_ADDR='/dt/ctl'
-HIVE_TBLES='t_xdr_event_msg business_type_detail tac_hour_http cell_hour_http sp_hour_http ue_hour_http imsi_cell_hour_http sgw_hour_http zc_city_data lte_cellmr_source'
+HIVE_TBLES='t_xdr_event_msg business_type_detail tac_hour_http cell_hour_http sp_hour_http ue_hour_http imsi_cell_hour_http sgw_hour_http zc_city_data'
 
 rm -rf ${LOCAL_ADDR}/${TAKING_DATE}
 mkdir ${LOCAL_ADDR}/${TAKING_DATE}
