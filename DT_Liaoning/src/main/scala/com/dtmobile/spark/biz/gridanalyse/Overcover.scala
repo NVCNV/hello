863982a8da6d724d50afc38f9ec21ded419447dd
gelongqiang <gelongqiang@datangmobile.cn>
2017-08-23 14:01:21 +0800

    [删除删除分区语句]

--- DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/gridanalyse/Overcover.scala
+++ DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/gridanalyse/Overcover.scala
@@ -71,9 +71,6 @@ class Overcover(ANALY_DATE: String, ANALY_HOUR: String, SDB: String, DDB: String
       AdjStrongDisturbRateThreshold = t6.get(0).getDecimal(0).doubleValue()*100
     }
     sql(
-      s"""alter table $DDB.lte_mrs_overcover_ana60 if exists drop partition(dt=$ANALY_DATE,h=$ANALY_HOUR)
-      """.stripMargin)
-    sql(
       s"""alter table $DDB.lte_mrs_overcover_ana60 add if not exists partition(dt=$ANALY_DATE,h=$ANALY_HOUR)
       """.stripMargin)
     sql(
