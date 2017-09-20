0019580b3b5679db94f634997498a7eebf2a584b
shenkaili <shenkaili@datangmobile.cn>
2017-09-20 10:41:24 +0800

    add railline

--- DT_Shanxi/shell/bin/highSpeed/bushu/CRH_IMSI_cell_hour_kpi_new.sh
+++ DT_Shanxi/shell/bin/highSpeed/bushu/CRH_IMSI_cell_hour_kpi_new.sh
@@ -7,7 +7,7 @@ DEFAULTDB=$4
 CAL_DATE="${ANALY_DATE:0:(4)}-${ANALY_DATE:4:(2)}-${ANALY_DATE:6:(2)} ${ANALY_HOUR}:00:00"
 
 uusql="
-select imsi,msisdn,cellid,(eupordown)dir_state,0 as voltemcsucc,0 as voltemcatt,0 as voltevdsucc,0 as voltevdatt,0 as voltetime,
+select imsi,msisdn,cellid,railline,(eupordown)dir_state,0 as voltemcsucc,0 as voltemcatt,0 as voltevdsucc,0 as voltevdatt,0 as voltetime,
 0 as voltemctime,0 as voltemctimey,0 as voltevdtime,0 as voltevdtimey,
 0 as voltemchandover,0 as volteanswer,0 as voltevdhandover,0 as voltevdanswer,0 as srvccsucc,0 as srvccatt,0 as srvcctime,
 sum(case when (proceduretype = 7 or proceduretype = 8) and procedurestatus=0 then 1 else 0 end)lteswsucc,
@@ -30,10 +30,10 @@ sum(case when ProcedureType=7 then 1 else 0 end)swy,
           from tb_xdr_ifc_uu_new
           WHERE dt="$ANALY_DATE" and h="$ANALY_HOUR"
                  group by 
-                  imsi,msisdn,CELLID,eupordown
+                  imsi,msisdn,CELLID,railline,eupordown
 "
 x2sql="
-select imsi,msisdn,CELLID,(eupordown)dir_state,0 as voltemcsucc,0 as voltemcatt,0 as voltevdsucc,0 as voltevdatt,0 as voltetime,
+select imsi,msisdn,CELLID,railline,(eupordown)dir_state,0 as voltemcsucc,0 as voltemcatt,0 as voltevdsucc,0 as voltevdatt,0 as voltetime,
 0 as voltemctime,0 as voltemctimey,0 as voltevdtime,0 as voltevdtimey,
 0 as voltemchandover,0 as volteanswer,0 as voltevdhandover,0 as voltevdanswer,
 0 as srvccsucc,0 as srvccatt,0 as srvcctime,
@@ -50,10 +50,10 @@ sum(case when ProcedureType=1 then 1 else 0 end)swy,
           from tb_xdr_ifc_x2_new
           WHERE dt="$ANALY_DATE" and h="$ANALY_HOUR"
                  group by 
-                  imsi,msisdn,CELLID,eupordown
+                  imsi,msisdn,CELLID,railline,eupordown
 "
 svsql="
-select imsi,msisdn,(SOURCEECI)cellid,(eupordown)dir_state,0 as voltemcsucc,0 as voltemcatt,0 as voltevdsucc,0 as voltevdatt,0 as voltetime,0 as voltemctime,0 as voltemctimey,0 as voltevdtime,0 as voltevdtimey,
+select imsi,msisdn,(SOURCEECI)cellid,railline,(eupordown)dir_state,0 as voltemcsucc,0 as voltemcatt,0 as voltevdsucc,0 as voltevdatt,0 as voltetime,0 as voltemctime,0 as voltemctimey,0 as voltevdtime,0 as voltevdtimey,
 0 as voltemchandover,0 as volteanswer,0 as voltevdhandover,0 as voltevdanswer,
 0 as srvccsucc,
 0 as srvccatt,
@@ -66,11 +66,11 @@ sum(case when ProcedureType=1 and RESULT=0 then SVDELAY/100 end)srvcctime,
           from tb_xdr_ifc_sv_new 
           WHERE dt="$ANALY_DATE" and h="$ANALY_HOUR"
                  group by 
-                  imsi,msisdn,SOURCEECI,eupordown
+                  imsi,msisdn,SOURCEECI,railline,eupordown
 "
 
 voltesipsql="
-select imsi,msisdn,(sourceeci)cellid,(eupordown)dir_state,
+select imsi,msisdn,(sourceeci)cellid,railline,(eupordown)dir_state,
 0 as voltemcsucc,
 0 as voltemcatt,
 0 as voltevdsucc,
@@ -95,11 +95,11 @@ sum(case when ProcedureType=1 and interface=14 and ProcedureStatus=0 then 1 else
           from tb_xdr_ifc_gmmwmgmimjisc_new
           WHERE dt="$ANALY_DATE" and h="$ANALY_HOUR"
                  group by 
-               imsi,msisdn,sourceeci,eupordown
+               imsi,msisdn,sourceeci,railline,eupordown
 "
 
 voltesip0sql="
-select imsi,msisdn,(sourceeci)cellid,(eupordown)dir_state,
+select imsi,msisdn,(sourceeci)cellid,railline,(eupordown)dir_state,
 sum(case when ProcedureType=5 and interface=14 and ServiceType=0 and alertingtime is not null then 1 else 0 end)voltemcsucc,
 sum(case when ProcedureType=5 and interface=14 and ServiceType=0 then 1 else 0 end)voltemcatt,
 sum(case when ProcedureType=5 and interface=14 and ServiceType=1 and alertingtime is not null then 1 else 0 end)voltevdsucc,
@@ -124,11 +124,11 @@ sum(case when interface=14 and ProcedureType=5 and alertingtime is not null then
           from tb_xdr_ifc_gmmwmgmimjisc_new
          where callside=0 and dt="$ANALY_DATE" and h="$ANALY_HOUR"
                  group by
-               imsi,msisdn,sourceeci,eupordown
+               imsi,msisdn,sourceeci,railline,eupordown
 "
 
 voltesip1sql="
-select imsi,msisdn,(desteci)cellid,(eupordown)dir_state,
+select imsi,msisdn,(desteci)cellid,railline,(eupordown)dir_state,
 sum(case when ProcedureType=5 and interface=14 and ServiceType=0 and alertingtime is not null then 1 else 0 end)voltemcsucc,
 sum(case when ProcedureType=5 and interface=14 and ServiceType=0 then 1 else 0 end)voltemcatt,
 sum(case when ProcedureType=5 and interface=14 and ServiceType=1 and alertingtime is not null then 1 else 0 end)voltevdsucc,
@@ -154,11 +154,11 @@ sum(case when interface=14 and ProcedureType=5 and alertingtime is not null then
           from tb_xdr_ifc_gmmwmgmimjisc_new
        where callside=1 and dt="$ANALY_DATE" and h="$ANALY_HOUR"
                  group by 
-                  imsi,msisdn,desteci,eupordown
+                  imsi,msisdn,desteci,railline,eupordown
 "
 
 #excel sheet����VOLTE_Rx�ӿ�С������
-#volterxsql="(select imsi,msisdn,ECGI,(eupordown)dir_state,0 as voltemcsucc, 0 as voltemcatt, 0 as voltevdsucc, 0 as voltevdatt, 0 as voltetime, 0 as voltemctime, 0 as voltevdtime, 
+#volterxsql="(select imsi,msisdn,ECGI,railline,(eupordown)dir_state,0 as voltemcsucc, 0 as voltemcatt, 0 as voltevdsucc, 0 as voltevdatt, 0 as voltetime, 0 as voltemctime, 0 as voltevdtime,
 #count(case when Interface = 26 and ProcedureType = 3 and (MEDIATYPE !=1 or MEDIATYPE is null) and AbortCause in (0, 1, 2, 4) then 1 end) voltemchandover, 0 as volteanswer, 
 #count(case when Interface = 26 and ProcedureType = 3 and MEDIATYPE = 1 and AbortCause in (0, 1, 2, 4) then 1 end) voltevdhandover, 0 as voltevdanswer,
 #0 as srvccsucc, 0 as srvccatt, 0 as srvcctime, 0 as lteswsucc, 0 as lteswatt, 0 as srqatt , 0 as srqsucc , 0 as tauatt  , 0 as tausucc, 0 as rrcrebuild, 
@@ -171,10 +171,10 @@ sum(case when interface=14 and ProcedureType=5 and alertingtime is not null then
 #          from TB_XDR_IFC_GXRX
 #           WHERE dt="$ANALY_DATE" and h="$ANALY_HOUR"
 #              group by 
-#                  imsi,msisdn,ECGI,dir_state)"
+#                  imsi,msisdn,ECGI,railline,dir_state)"
 
 
-s1mmesql="select imsi,msisdn,CELLID,(eupordown)dir_state,0 as voltemcsucc, 0 as voltemcatt, 0 as voltevdsucc, 0 as voltevdatt, 
+s1mmesql="select imsi,msisdn,CELLID,railline,(eupordown)dir_state,0 as voltemcsucc, 0 as voltemcatt, 0 as voltevdsucc, 0 as voltevdatt,
 0 as voltetime, 0 as voltemctime,0 as voltemctimey,0 as voltevdtime, 0 as voltevdtimey,0 as voltemchandover, 0 as volteanswer, 
 0 as voltevdhandover, 0 as voltevdanswer,
 sum(case when PROCEDURETYPE=16 and keyword1=3 and PROCEDURESTATUS=0 then 1 else 0 end)srvccsucc,
@@ -206,10 +206,10 @@ sum(case when INTERFACE = 5 and proceduretype = 1 then 1 else 0 end ) attachy,
           from tb_xdr_ifc_s1mme_new T
           WHERE dt="$ANALY_DATE" and h="$ANALY_HOUR"
                group by 
-                  imsi,msisdn,cellid,eupordown"
+                  imsi,msisdn,cellid,railline,eupordown"
 
 S1MMEHANDOVER="
-select imsi,msisdn,CELLID,(eupordown)dir_state,
+select imsi,msisdn,CELLID,railline,(eupordown)dir_state,
 0 as voltemcsucc, 
 0 as voltemcatt, 
 0 as voltevdsucc, 
@@ -262,7 +262,7 @@ from
 (select * from tb_xdr_ifc_s1mme_new where dt="$ANALY_DATE" and h="$ANALY_HOUR" and PROCEDURETYPE = 16 and keyword1 = 1 AND PROCEDURESTATUS = 0 AND IMSI is not NULL) S1MME_1 left join
 (select * from tb_xdr_ifc_s1mme_new where dt="$ANALY_DATE" and h="$ANALY_HOUR" and PROCEDURETYPE = 20  and requestcause = 2 AND IMSI is not NULL) S1MME_2
 on S1MME_1.IMSI = S1MME_2.IMSI AND S1MME_1.CELLID = S1MME_2.CELLID where S1MME_2.PROCEDURESTARTTIME between S1MME_1.PROCEDURESTARTTIME and S1MME_1.PROCEDURESTARTTIME + 5*1000) a
-group by imsi,msisdn,CELLID,eupordown
+group by imsi,msisdn,CELLID,railline,eupordown
 "
 
 hive<<EOF
@@ -272,12 +272,12 @@ alter table kpi_mid_imsi_cell_hour drop partition(dt="$ANALY_DATE",h="$ANALY_HOU
 alter table kpi_mid_imsi_cell_hour add partition(dt="$ANALY_DATE",h="$ANALY_HOUR");
 
 insert into table kpi_mid_imsi_cell_hour partition(dt="$ANALY_DATE",h="$ANALY_HOUR")
-(ttime,imsi,msisdn,cellid,dir_state,kpi031,kpi032,kpi033,kpi034,kpi035,kpi037,kpi039,kpi041,
+(ttime,imsi,msisdn,cellid,railline,dir_state,kpi031,kpi032,kpi033,kpi034,kpi035,kpi037,kpi039,kpi041,
 kpi042,kpi043,kpi044,kpi045,kpi046,kpi047,kpi009,kpi010,kpi020,kpi019,kpi022,kpi021,kpi001,
 kpi003,kpi004,kpi029,kpi030,kpi023,kpi024,kpi025,kpi026,kpi013,kpi014,kpi017,kpi018,kpi011,kpi012,kpi005,
 kpi006,kpi007,kpi008,kpi015,kpi016,kpi027,kpi028, kpi071,kpi072,kpi079,kpi075
 )
-select "$CAL_DATE",imsi,msisdn,CELLID,dir_state,sum(voltemcsucc),sum(voltemcatt),sum(voltevdsucc),sum(voltevdatt),sum(voltetime),
+select "$CAL_DATE",imsi,msisdn,CELLID,railline,dir_state,sum(voltemcsucc),sum(voltemcatt),sum(voltevdsucc),sum(voltevdatt),sum(voltetime),
 sum(voltemctime),sum(voltemctimey),sum(voltevdtime),sum(voltevdtimey),sum(voltemchandover),sum(volteanswer),sum(voltevdhandover),sum(voltevdanswer),sum(srvccsucc),
 sum(srvccatt),sum(srvcctime),sum(lteswsucc),sum(lteswatt),sum(srqatt),sum(srqsucc),sum(tauatt),sum(tausucc),sum(rrcrebuild),
 sum(rrcsucc),sum(rrcreq),sum(imsiregatt),sum(imsiregsucc),sum(wirelessdrop),sum(wireless),sum(eabdrop),sum(eab),sum(eabs1swx),
@@ -301,6 +301,6 @@ from
  union
  $S1MMEHANDOVER
 ) t7
-group by imsi,msisdn,cellid,dir_state;
+group by imsi,msisdn,cellid,railline,dir_state;
 EOF
 
