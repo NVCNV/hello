7784cc7837829da19e338d49b8176662ec4617b3
gelongqiang <gelongqiang@datangmobile.cn>
2017-09-30 09:52:01 +0800

    [新增lte_cellmr_source sqlldr]

--- DT_Shanxi/shell/bin/highSpeed/NewEtypeToOracle.sh
+++ DT_Shanxi/shell/bin/highSpeed/NewEtypeToOracle.sh
@@ -3,6 +3,7 @@
 TAKING_DATE=$1
 TAKING_HOUR=$2
 ORACLEDB=$3
+RESULTDB=$4
 HDFS_ADDR=" hdfs://dtcluster/datang2/ETYPE_OUT/${TAKING_DATE}/${TAKING_HOUR}"
 
 DB_ADDR="userid=scott/tiger@${ORACLEDB}"
@@ -34,6 +35,11 @@ echo "--------hdfs dfs -getmerge ${HDFS_ADDR}/TB_XDR_IFC_X2* ${LOCALDIR}/${TAKIN
 hdfs dfs -getmerge ${HDFS_ADDR}/TB_XDR_IFC_X2* ${LOCALDIR}/${TAKING_DATE}/${TAKING_HOUR}/tb_xdr_ifc_x2.dat
 mkdir -p /dt/sqlldrLog/tb_xdr_ifc_x2/${TAKING_DATE}
 sqlldr ${DB_ADDR} control=${CTLDIR}/tb_xdr_ifc_x2.ctl data=${LOCALDIR}/${TAKING_DATE}/${TAKING_HOUR}/tb_xdr_ifc_x2.dat log=/dt/sqlldrLog/tb_xdr_ifc_x2/${TAKING_DATE}/${TAKING_HOUR}
+
+echo "----------hdfs dfs -getmerge /user/hive/warehouse/${RESULTDB}.db/lte_cellmr_source/${TAKING_DATE}/${TAKING_HOUR}/* ${LOCALDIR}/${TAKING_DATE}/${TAKING_HOUR}/lte_cellmr_source.dat-----------"
+hdfs dfs -getmerge /user/hive/warehouse/${RESULTDB}.db/lte_cellmr_source/dt=${TAKING_DATE}/h=${TAKING_HOUR}/* ${LOCALDIR}/${TAKING_DATE}/${TAKING_HOUR}/lte_cellmr_source.dat
+mkdir -p /dt/sqlldrLog/lte_cellmr_source/${TAKING_DATE}
+sqlldr ${DB_ADDR} control=${CTLDIR}/lte_cellmr_source.ctl data=${LOCALDIR}/${TAKING_DATE}/${TAKING_HOUR}/lte_cellmr_source.dat log=/dt/sqlldrLog/lte_cellmr_source/${TAKING_DATE}/${TAKING_HOUR}
 
 exit 0
 
