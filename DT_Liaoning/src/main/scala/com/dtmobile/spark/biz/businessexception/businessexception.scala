2a40e976380751e587650ad300a526fbbdd786c5
gelongqiang <gelongqiang@datangmobile.cn>
2017-10-25 17:09:26 +0800

    [修改Morpho MORPHO-6685 bug]

--- DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businessexception/businessexception.scala
+++ DT_Liaoning/src/main/scala/com/dtmobile/spark/biz/businessexception/businessexception.scala
@@ -220,7 +220,7 @@ class businessexception (ANALY_DATE: String,ANALY_HOUR: String,SDB: String, DDB:
           |on t.cellid = s.cellid
           |where t.dt=$ANALY_DATE and t.h=$ANALY_HOUR
           |group by t.cellid,t.etype,t.city)t2
-          |where t2.speed >$tiemdelay/100 or t2.delay>$vsdelay/100 or t2.inst>$timeandvsdelay/100
+          |where t2.delay >$tiemdelay/100 or t2.speed>$vsdelay/100 or t2.inst>$timeandvsdelay/100
        """.stripMargin).write.mode(SaveMode.Overwrite).csv(s"$warhouseDir/zc_city_data/dt=$ANALY_DATE/h=$ANALY_HOUR")
   }
 
